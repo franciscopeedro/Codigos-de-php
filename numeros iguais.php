92f621e7e08354df348a4bf0a28153b69b0d72a0
Francisco Pedro <109813896+franciscopeedro@users.noreply.github.com>
2022-09-10 10:23:26 -0300

    finish

---
+++ numeros iguais.php
@@ -0,0 +1,15 @@
+<?php 
+
+$primeiro=1;
+$segundo=2;
+
+if($primeiro==$segundo){
+    echo "Os numeros sao iguais!" . PHP_EOL;
+}else if($primeiro>$segundo){
+    echo "O primeiro numero eh maior!" . PHP_EOL;
+}else{
+    echo "O segundo numero eh maior!" . PHP_EOL;
+}
+
+
+?>
